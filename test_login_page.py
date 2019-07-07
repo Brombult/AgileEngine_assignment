36f9e895408ddf29fa1a71373df0a6cf8bba3d73
Boris R <rickrolled5997@gmail.com>
2019-07-07 15:10:17 +0300

    improve login tests with main page checks

--- test_login_page.py
+++ test_login_page.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import pytest
 
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 load_dotenv()  # loading .env file
 
@@ -30,11 +31,19 @@ class TestLoginPage:
         login_page.login(email=VALID_EMAIL, password=VALID_PASSWORD)
         login_page.ensure_login_failed_message_is_not_present()
 
+        main_page = MainPage(browser, browser.current_url)
+        main_page.ensure_main_page_is_open()
+        main_page.ensure_that_sign_in_message_is_displayed()
+
     def test_login_with_valid_credentials_and_remember_me(self, browser):
         login_page = LoginPage(browser, LOGIN_PAGE_URL)
         login_page.open()
         login_page.login_with_remember_me(email=VALID_EMAIL, password=VALID_PASSWORD)
         login_page.ensure_login_failed_message_is_not_present()
+
+        main_page = MainPage(browser, browser.current_url)
+        main_page.ensure_main_page_is_open()
+        main_page.ensure_that_sign_in_message_is_displayed()
 
     def test_login_with_invalid_credentials(self, browser):
         login_page = LoginPage(browser, LOGIN_PAGE_URL)
